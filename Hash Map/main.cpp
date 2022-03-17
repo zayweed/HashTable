656377cf83132df46e8a9606b7d4f1bad7af16bb
zayweed <91290511+zayweed@users.noreply.github.com>
2022-03-17 02:36:41 -0700

    Add files via upload

--- Hash Map/main.cpp
+++ Hash Map/main.cpp
@@ -1,5 +1,9 @@
 /*
-Credits to Nihal for the 'first.txt' and 'last.txt' files.
+Zayeed Saffat
+3/16/2022
+This project uses a hash table and random student generator to efficiently store student data using a hash function. 
+The commands are 'ADD', 'PRINT', 'DELETE', and 'QUIT'.
+Credits to Nihal Parthasarathy for the 'first.txt' and 'last.txt' files.
 */
 
 #include <bits/stdc++.h>
@@ -55,6 +59,7 @@ void print(Node* head);
 void del(Node* &head, Node* current, Node* previous, int id);
 int hashF(Student* student, int size);
 void randomStudents(Node**& table, int num, int& id, int size);
+int collisionCount(Node** table, int size);
 
 
 int main() {
@@ -79,7 +84,28 @@ int main() {
             int num; cin >> num;
 
             randomStudents(table, num, id, size);
-            cout << num << endl;
+    
+            if (collisionCount(table, size) > 3) { //if are more than 3 collisions somewhere in the hash table
+                //make a temporary array that is double the size
+                size = size*2;
+
+                Node** temp = new Node*[size];
+                for (int i = 0; i < size; i++) {
+                    temp[i] = NULL;
+                }
+
+                for (int i = 0; i < size/2; i++) { //rehash
+                    Node* node = table[i];
+                    while (node != NULL) {
+                        add(temp[hashF(node->getStudent(), size)], node->getStudent());
+                        node = node->getNext();
+                    }
+                }
+
+                //replace the hash table with the temporary table
+                table = new Node*[size];
+                table = temp;
+            }
         }
 
         if (strcmp(input, "PRINT") == 0) { //print
@@ -97,7 +123,7 @@ int main() {
             }
         }
 
-        if (strcmp(input, "QUIT") == 0) {
+        if (strcmp(input, "QUIT") == 0) { //quit
             stillPlaying = false;
         }
     }
@@ -106,10 +132,10 @@ int main() {
 //function that adds student to end of current linked list
 void add(Node*& head, Student* student) {
     Node* current = head;
-    if(current == NULL) {
+    if(current == NULL) { //sets new to head if head is null
         head = new Node(student);
     } 
-    else {
+    else { //adds new node to end of the linked list
         while (current->getNext() != NULL) {
         current = current->getNext();
         }
@@ -117,7 +143,7 @@ void add(Node*& head, Student* student) {
     }
 }
 
-//function that prints all students stored in the table
+//function that prints all students stored from one head
 void print(Node* head) {
     if (head != NULL) {
         while(head != NULL) {
@@ -132,30 +158,28 @@ void print(Node* head) {
     }
 }
 
-//function that deletes a student
+//function that deletes a student by id number
 void del(Node* &head, Node* current, Node* previous, int id) {
-    if (head == NULL) {
+    if (head == NULL) { //does nothing if head is null
         return;
     } 
 
-    else if (current == NULL) {
+    else if (current == NULL) { //does nothing if the current node is null
         return;
     } 
 
-    else if (id == current->getStudent()->id) {
+    else if (id == current->getStudent()->id) { //removes current node if target id is found
         if (id == head->getStudent()->id) {
             Node* temp = head;
             head = head->getNext();
-            temp->~Node();
         } 
         else {
             previous->setNext(current->getNext());
-            current->~Node();
         }
     } 
 
     else {
-        del(head, current->getNext(), current, id);
+        del(head, current->getNext(), current, id); //recursion
     }
 }
 
@@ -168,9 +192,11 @@ int hashF(Student* student, int size) {
 void randomStudents(Node**& table, int num, int& id, int size) {
     srand(time(NULL));
     for (int i = 0; i < num; i++) {
+        //generates 2 random numbers from 0 to 19 for which line of the 'first.txt' and 'last.txt' to get the names from
         int firstline = rand() % 20+1;
         int lastline = rand() % 20+1;
 
+        //file stuff
         fstream fin; 
         fstream lin;
         fin.open("first.txt");
@@ -185,11 +211,29 @@ void randomStudents(Node**& table, int num, int& id, int size) {
             lin >> last;
         }
 
-        float GPA = float(rand()%501)/100;
+        float GPA = float(rand()%501)/100; //generates random float from 0.00 to 5.00 to use as gpa
 
-        Student* student = new Student(first, last, id, GPA);
-        id++;
+        Student* student = new Student(first, last, id, GPA); //makes the student
+        id++; //increments id
 
-        add(table[hashF(student, size)], student);
+        add(table[hashF(student, size)], student); //adds student to table
     }
+}
+
+//function that returns the max number of collisions found in the table
+int collisionCount(Node** table, int size) {
+    int maximum = 0;
+    for (int i = 0; i < size; i++) {
+        Node* node = table[i];
+        int current = 1;
+        if (node != NULL) {
+            while (node->getNext() != NULL) {
+                node = node->getNext();
+                current++;
+            }
+            maximum = max(maximum, current);
+        }
+    }
+
+    return maximum;
 }
