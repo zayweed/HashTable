fecdd26c2cf506ddb832fa50443a3019c81a1efe
zayweed <91290511+zayweed@users.noreply.github.com>
2022-03-17 01:15:11 -0700

    Add files via upload

--- Hash Map/main.cpp
+++ Hash Map/main.cpp
@@ -3,11 +3,10 @@ Credits to Nihal for the 'first.txt' and 'last.txt' files.
 */
 
 #include <bits/stdc++.h>
-#include <string>
 
 using namespace std;
 
-struct Student {
+struct Student { //student struct
     char first[100];
     char last[100];
     int id;
@@ -22,7 +21,7 @@ struct Student {
     }
 };
 
-class Node {
+class Node { //node class
     Student* student;
     Node* next;
 
@@ -50,20 +49,75 @@ class Node {
 
 };
 
-//function that adds student to student list
-void add(Node*& head, Student* student) {
-    Node* newNode = new Node(student);
-    Node* current = head;
-    if (current == NULL) {
-        head = newNode;
+//function prototypes
+void add(Node*& head, Student* student);
+void print(Node* head);
+void del(Node* &head, Node* current, Node* previous, int id);
+int hashF(Student* student, int size);
+void randomStudents(Node**& table, int num, int& id, int size);
+
+
+int main() {
+    cout << "START" << endl;
+
+    int size = 100;
+    Node** table = new Node*[size];
+    for (int i = 0; i < size; i++) {
+        table[i] = NULL;
     }
-    while (current->getNext() != NULL) {
-        current = current->getNext();
-    } 
-    current->setNext(newNode);
+    int id = 0;
+
+    bool stillPlaying = true;
+
+    while (stillPlaying == true) {
+        cout << "Would you like to 'ADD', 'PRINT', 'DELETE', or 'QUIT'" << endl;
+        char input[100];
+        cin >> input;
+
+        if (strcmp(input, "ADD") == 0) { //add
+            cout << "How many students would you like to add?" << endl;
+            int num; cin >> num;
+
+            randomStudents(table, num, id, size);
+            cout << num << endl;
+        }
+
+        if (strcmp(input, "PRINT") == 0) { //print
+            for (int i = 0; i < size; i++) {
+                print(table[i]);
+            }
+        }
+
+        if (strcmp(input, "DELETE") == 0) { //delete 
+            cout << "What is the id of the student you want to delete?" << endl;
+            int id; cin >> id;
+        
+            for (int i = 0; i < size; i++) {
+                del(table[i], table[i], table[i], id);
+            }
+        }
+
+        if (strcmp(input, "QUIT") == 0) {
+            stillPlaying = false;
+        }
+    }
 }
 
-//function that prints all students stored in the studentlist
+//function that adds student to end of current linked list
+void add(Node*& head, Student* student) {
+    Node* current = head;
+    if(current == NULL) {
+        head = new Node(student);
+    } 
+    else {
+        while (current->getNext() != NULL) {
+        current = current->getNext();
+        }
+        current->setNext(new Node(student));
+    }
+}
+
+//function that prints all students stored in the table
 void print(Node* head) {
     if (head != NULL) {
         while(head != NULL) {
@@ -71,7 +125,7 @@ void print(Node* head) {
             << head->getStudent()->first << " "
             << head->getStudent()->last << ", "
             << head->getStudent()->id << ", "
-            << head->getStudent()->GPA;
+            << head->getStudent()->GPA << endl;
 
             head = head->getNext();
         }
@@ -79,45 +133,45 @@ void print(Node* head) {
 }
 
 //function that deletes a student
-void del(Node**& table, int id, int size) {
-    for (int i = 0; i < size; i++) {
-        Node* head = table[i];
+void del(Node* &head, Node* current, Node* previous, int id) {
+    if (head == NULL) {
+        return;
+    } 
 
-        if (head != NULL) {
-            if (head->getStudent()->id == id) {
-                Node* temp = head;
-                head = head->getNext();
-                temp->~Node();
-            }
-            else {
-                Node* previous = head;
-                Node* current = head;
+    else if (current == NULL) {
+        return;
+    } 
 
-                while (current->getNext() != NULL) {
-                    current = current->getNext();
-                    if (current->getStudent()->id == id) {
-                        previous->setNext(current->getNext());
-                        current->~Node();
-                        return;
-                    }
-                    previous = current;
-                }
-            }
+    else if (id == current->getStudent()->id) {
+        if (id == head->getStudent()->id) {
+            Node* temp = head;
+            head = head->getNext();
+            temp->~Node();
+        } 
+        else {
+            previous->setNext(current->getNext());
+            current->~Node();
         }
+    } 
+
+    else {
+        del(head, current->getNext(), current, id);
     }
 }
 
+//hash function
 int hashF(Student* student, int size) {
     return student->id % size;
 }
 
-void generate(Node**& table, int num, int& id, int size) {
+//function that generates random students and adds them to the hash table
+void randomStudents(Node**& table, int num, int& id, int size) {
     srand(time(NULL));
     for (int i = 0; i < num; i++) {
         int firstline = rand() % 20+1;
         int lastline = rand() % 20+1;
 
-        fstream fin;
+        fstream fin; 
         fstream lin;
         fin.open("first.txt");
         lin.open("last.txt");
@@ -137,48 +191,5 @@ void generate(Node**& table, int num, int& id, int size) {
         id++;
 
         add(table[hashF(student, size)], student);
-    }
-}
-
-int main() {
-    cout << "START" << endl;
-
-    int size = 100;
-    Node** table = new Node*[size];
-    for (int i = 0; i < size; i++) {
-        table[i] = NULL;
-    }
-    int id = 0;
-
-    bool stillPlaying = true;
-
-    while (stillPlaying == true) {
-        cout << "Would you like to 'ADD', 'PRINT', 'DELETE', or 'QUIT'" << endl;
-        char input[100];
-        cin >> input;
-
-        if (strcmp(input, "ADD") == 0) {
-            cout << "How many students would you like to add?" << endl;
-            int num; cin >> num;
-
-            generate(table, num, id, size);
-        }
-
-        if (strcmp(input, "PRINT") == 0) {
-            for (int i = 0; i < size; i++) {
-                print(table[i]);
-            }
-        }
-
-        if (strcmp(input, "DELETE") == 0) {
-            cout << "What is the id of the student you want to delete?" << endl;
-            int id; cin >> id;
-        
-            del(table, id, size);
-        }
-
-        if (strcmp(input, "QUIT") == 0) {
-            stillPlaying = false;
-        }
     }
 }
